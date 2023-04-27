362d1d24d64971936078628b92053a33a74a8180
tessam30 <tim.essam@gmail.com>
2023-04-27 09:07:53 -0400

    feat: create gt theme for phc tables

--- Scripts/00_helper_functions.R
+++ Scripts/00_helper_functions.R
@@ -119,10 +119,13 @@ get_countryname <- function(df){
 }
 
 
-create_phc_gt <- function(df){
+create_phc_gt <- function(df, cntry = ""){
+  
+  cntry <- str_to_upper(cntry)
+  
   df %>% 
     select(order(colnames(.))) %>% 
-    relocate(total_sites, .after = 8) %>% 
+    relocate(total_sites, .after = last_col()) %>% 
     gt() %>% 
     fmt_number(columns = c(2, 4, 6), 
                decimals = 0) %>% 
@@ -397,3 +400,65 @@ summary_tbl_ou_starter <- function(df, viz_type) {
     )
   
 }
+
+# Custom theme for tables
+
+gt_theme_phc <- function (gt_object, ...) 
+{
+  stopifnot(`'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?` = "gt_tbl" %in% 
+              class(gt_object))
+  
+  gt_object %>%
+    tab_options(
+      heading.align = "left", 
+      #column_labels.border.top.style = "none",
+      table.border.top.style = "none", 
+      #column_labels.border.bottom.style = "none",
+      column_labels.border.bottom.width = 1, 
+      column_labels.border.bottom.color = "#334422",
+      #table_body.border.top.style = "none", 
+      table_body.border.bottom.color = "white",
+      #heading.border.bottom.style = "none", 
+      data_row.padding = px(7),
+      column_labels.font.size = px(12), ...
+    ) %>%
+    tab_style(
+      style = cell_text(
+        color = "darkgrey",
+        font = google_font("Source Sans Pro"), transform = "uppercase"
+      ),
+      locations = cells_column_labels(everything())
+    ) %>%
+    tab_style(
+      style = cell_text(
+        color = grey60k,
+        weight = "600",
+        font = google_font("Source Sans Pro"), transform = "uppercase"
+      ),
+      locations = cells_column_spanners(spanners = everything())
+    ) %>%
+    tab_style(style = cell_text(
+      font = google_font("Libre Franklin"),
+      weight = 800
+    ), 
+    locations = cells_title(groups = "title")) %>%
+    tab_style(style = cell_text(
+      font = google_font("Source Sans Pro"),
+      weight = 400
+    ), 
+    locations = cells_body()
+    # ) %>% 
+    # tab_style(style = cell_text(
+    #   font = google_font("Source Sans Pro"),
+    #   weight = 400
+    # ), 
+    # locations = cells_summary()
+    ) %>%
+    tab_style(style = cell_text(
+      font = google_font("Source Sans Pro"),
+      weight = 600
+    ), 
+    locations = cells_grand_summary()
+    )
+}
+
--- Scripts/108_msd_indicator_summary_by_facility_type.R
+++ Scripts/108_msd_indicator_summary_by_facility_type.R
@@ -18,6 +18,9 @@
     library(patchwork)
     library(ggtext)
     library(glue)
+    library(googledrive)
+    library(gtExtras)
+    library(gt)
     
     
   # SI specific paths/functions  
@@ -31,6 +34,8 @@
   # Use google drive to download file and then pass temp path through to vroom to open
     temp <- tempfile(fileext = ".zip")
     dl <- drive_download(as_id(ss_sites), path = temp, overwrite = T)
+    
+    get_metadata("Data/MER_Structured_Datasets_Site_IM_FY21-23_20230317_v2_1_Botswana.zip")
       
   # REF ID for plots
     ref_id <- "cce714cf"
@@ -73,76 +78,208 @@
    df_phc <- df_daa %>% 
       tidylog::left_join(., df_api, by = c("orgunit_internal_id" = "orgunituid")) %>% 
       mutate(merge_daa_api = ifelse(is.na(pepfar_fp), "DAA only", "DAA & API")) %>% 
-      tidylog::left_join(., df_daa_sa, by = c("orgunit_internal_id" = "orgunit"))%>% 
+      tidylog::left_join(., df_daa_sa, by = c("orgunit_internal_id" = "orgunit")) %>% 
       mutate(merge_daa_sa = ifelse(is.na(dataelement), "DAA only", "DAA & SA")) %>% 
       collapse_fac_type() %>% 
-      relevel_fac_type() %>% 
-      mutate(pepfar_fp_adj = ifelse(is.na(pepfar_fp), "Non-PEPFAR", pepfar_fp),
-             pepfar_fp_adj = fct_relevel(pepfar_fp_adj, c("PEPFAR", "Non-PEPFAR")))
-   
+      relevel_fac_type() 
     
-  
-# VIZ ============================================================================
-
-  # Pick a country and document process
-    
-    df_msd %>% filter(operatingunit == "Uganda") %>% 
+    df_msd_phc <-  
+      df_msd %>% 
       left_join(., df_phc, by = c("orgunituid" = "orgunit_internal_id")) %>% 
       mutate(fac_type = case_when(
-               str_detect(facility, "Data reported") ~ "Above site", 
-               TRUE ~ fac_type)
+        str_detect(facility, "Data reported") ~ "Above site", 
+        TRUE ~ fac_type)
       ) %>% 
-      relevel_fac_type() %>% 
-      mutate(age_bands = fct_relevel(age_bands, c("Pediatrics", "AYPs", "Adults"))) %>% 
-      group_by(fac_type, indicator, age_bands, sex) %>% 
-      summarise(indic_value = sum(indic_value, na.rm = T)) %>% 
-      mutate(indicator = str_c(indicator, sex, sep = "_")) %>% 
-      select(-sex) %>% 
-      spread(indicator, indic_value) %>% 
-      arrange(age_bands, fac_type) %>% 
-      gt(groupname_col = "age_bands") %>% 
-      summary_rows(groups = c("Pediatrics", "AYPs", "Adults"),
-                   columns = 2:8,
-                   fns = list(
-                     subtotal = ~sum(., na.rm = T)),
-                   formatter = fmt_number,
-                   decimals = 0
-      ) %>% 
-      grand_summary_rows(columns = is.numeric,
-                              fns = list(
-                                Overall = ~sum(., na.rm = T)), 
-                              formatter = fmt_number,
-                              decimals = 0
-      ) %>% 
-      sub_missing(missing_text = ".") %>% 
-      gt_theme_nytimes() %>% 
-      fmt_number(columns = is.numeric,
-                 decimals = 0) %>% 
-      cols_label(fac_type = "", 
-                 HTS_TST_POS_Female = "Female",
-                 HTS_TST_POS_Male = "Male",
-                 TX_CURR_Female = "Female",
-                 TX_CURR_Male = "Male",
-                 TX_NEW_Female = "Female", 
-                 TX_NEW_Male = "Male") %>% 
-      tab_spanner(columns = 3:4,
-                  label = "HTS_TST_POS") %>% 
-      tab_spanner(columns = 5:6,
-                  label = "TX_CURR") %>% 
-      tab_spanner(columns = 7:8,
-                  label = "TX_NEW") %>% 
-      tab_options(
-        table.font.size = px(12),
-        column_labels.font.size = px(14),
-        row_group.font.weight = "bold",
-        row_group.font.size = px(14),
-        data_row.padding = px(1), 
-        summary_row.padding = px(1.5),
-        grand_summary_row.padding = px(1.5),
-      ) %>% 
-      row_highlight(rowvar = fac_type) %>% 
-      cols_align(align = "left", columns = 1) 
-      
+      relevel_fac_type() 
+    
+# VIZ ============================================================================
+    
+    make_msd_tbl <- function(df, ou){
+      df %>% 
+        filter(operatingunit == ou) %>% 
+        mutate(age_bands = fct_relevel(age_bands, c("Pediatrics", "AYPs", "Adults"))) %>% 
+        group_by(fac_type, indicator, age_bands, sex) %>% 
+        summarise(indic_value = sum(indic_value, na.rm = T)) %>% 
+        mutate(indicator = str_c(indicator, sex, sep = "_")) %>% 
+        select(-sex) %>% 
+        spread(indicator, indic_value) %>% 
+        arrange(age_bands, fac_type) %>% 
+        gt(groupname_col = "age_bands") %>% 
+        summary_rows(groups = c("Pediatrics", "AYPs", "Adults"),
+                     columns = 2:8,
+                     fns = list(
+                       subtotal = ~sum(., na.rm = T)),
+                     formatter = fmt_number,
+                     decimals = 0
+        ) %>% 
+        grand_summary_rows(columns = is.numeric,
+                                fns = list(
+                                  overall = ~sum(., na.rm = T)), 
+                                formatter = fmt_number,
+                                decimals = 0
+        ) %>% 
+        sub_missing(missing_text = ".") %>% 
+        fmt_number(columns = is.numeric,
+                   decimals = 0) %>% 
+        cols_label(fac_type = "", 
+                   HTS_TST_POS_Female = "Female",
+                   HTS_TST_POS_Male = "Male",
+                   TX_CURR_Female = "Female",
+                   TX_CURR_Male = "Male",
+                   TX_NEW_Female = "Female", 
+                   TX_NEW_Male = "Male") %>% 
+        tab_spanner(columns = 3:4,
+                    label = "HTS_TST_POS") %>% 
+        tab_spanner(columns = 5:6,
+                    label = "TX_CURR") %>% 
+        tab_spanner(columns = 7:8,
+                    label = "TX_NEW") %>% 
+        gt_theme_phc() %>% 
+        tab_options( # Adjusting padding between rows and font sizes
+          table.font.size = px(12),
+          column_labels.font.size = px(14),
+          row_group.font.weight = "bold",
+          row_group.font.size = px(14),
+          data_row.padding = px(1), 
+          summary_row.padding = px(1.5),
+          grand_summary_row.padding = px(1.5),
+          row_group.padding = px(2),
+          heading.padding = px(1)
+        ) %>% 
+        row_highlight(rowvar = fac_type) %>% 
+        cols_align(align = "left", columns = 1) %>% 
+        tab_header(
+          title = glue::glue("{str_to_upper(ou)} FY23 Q1 SUMMARY OF CORE INDICATORS BY FACILITY TYPE")
+        ) %>% 
+        tab_source_note(
+          source_note = gt::md(glue::glue("DATIM Data Alignment Activity Attribute Data & DATIM API 2023"))
+        ) 
+    }
+
+  # Test age/sex summary table 
+  make_msd_tbl(df_msd_phc, ou = "Zambia")
+    
+  # Batch and save
+  map(ou_list, ~make_msd_tbl(df_msd_phc, ou = .x) %>% 
+        gtsave(filename = glue("Images/{.x}_msd_fac_summary.png")))
+          
 
 # SPINDOWN ============================================================================
 
+  # Create facility / indicator summary tables with indicators rolled up to fac type
+  # Need to create a base table + site counts to be joined on
+  # Get site counts by facility type
+
+  ou_mer_site_count <- 
+    df_msd_phc %>% 
+    distinct(orgunituid, fac_type, operatingunit) %>% 
+    count(operatingunit, fac_type, name = "total_sites") %>% 
+    mutate(total_sites = ifelse(fac_type == "Above site", NA_integer_, total_sites))
+    
+ # Full share table for looping over    
+  ou_sh_tbl_full <-  
+    df_msd_phc %>% 
+      summarise(indic_value = sum(indic_value, na.rm = T), .by = c(operatingunit, fac_type, indicator)) %>% 
+      spread(indicator, indic_value) %>% 
+      group_by(operatingunit) %>% 
+      mutate(across(c(where(is.double)), ~(.x / sum(.x, na.rm = T)), .names = "{.col}_share")) %>% 
+      select(operatingunit, fac_type, order(colnames(.))) %>% 
+    left_join(., ou_mer_site_count) 
+
+
+  # Create a function to make summary rollup of MER indicators by OU
+  # This will be batched over and should fit into the existing
+  # `create_phc_gt()` function
+  
+    make_mer_sh_tbl <- function(df, ou){
+    
+      cntry <- str_to_upper(ou)
+      
+      df %>% 
+        filter(operatingunit == ou) %>%  
+        ungroup() %>% 
+        select(-operatingunit) %>% 
+        rename(facility_type = fac_type) %>% 
+        create_phc_gt(cntry = ou) %>% 
+        fmt_percent(columns = c(3, 5, 7), decimals = 0) %>% 
+        fmt_number(columns = 8, decimals = 0) %>% 
+        sub_missing(missing_text = ".") %>% 
+        cols_label(total_sites = "Total Sites Reporting") %>% 
+        gt_highlight_rows(
+          rows = facility_type %in% c("Health Post", "Primary Health Center"),
+          fill = grey10k,
+          font_weight = 600,
+          alpha = 0.45
+        ) %>% 
+        tab_options(
+          column_labels.font.size = px(15)
+        ) %>% 
+        grand_summary_rows(
+          columns = c(2, 4, 6, 8),
+            fns = list(
+              Overall = ~sum(., na.rm = T)), 
+            formatter = fmt_number,
+            decimals = 0
+      ) 
+    
+    }
+
+  # Pick an OU and test, then batch
+  make_mer_sh_tbl(ou_sh_tbl_full, "Uganda") 
+  
+  map(ou_list, ~make_mer_sh_tbl(ou_sh_tbl_full, .x) %>% 
+        gtsave(filename = glue("Images/{.x}_msd_overall_summary.png")))
+
+  
+  df_phc %>% 
+    filter(regionorcountry_name == "Uganda") %>% 
+    count(value)
+  
+  
+
+# Summary Table of Site Attributes by OU ----------------------------------
+
+  df_phc %>% 
+    count(regionorcountry_name, value) %>% 
+    mutate(value = ifelse(is.na(value), "Missing info", value)) %>% 
+    spread(value, n) %>% 
+    relocate(c(3, 4, 8), .after = 1) %>% 
+    relocate(4, .after = 1) %>% 
+    relocate(6, .after = last_col()) %>% 
+    arrange(desc(`Primary Health Center`)) %>% 
+    mutate(cntry_grp = case_when(
+      str_detect(regionorcountry_name, "Moz|Malawi|Zimb|Zam|South Africa|Uga") ~ "Large TX OUs",
+      TRUE ~ "Small TX OUs"
+    ),
+      cntry_grp = fct_relevel(cntry_grp, c("Large TX OUs", "Small TX OUs"))) %>% 
+    arrange(cntry_grp) %>% 
+    gt(groupname_col = "cntry_grp",
+       rowname_col  = "regionorcountry_name") %>% 
+    fmt_number(columns = is.numeric, decimals = 0) %>% 
+    tab_spanner(columns = c(2, 3), 
+                label = "focus",gather = T) %>% 
+    grand_summary_rows(
+      columns = is.numeric,
+      fns = list(
+        Overall = ~sum(., na.rm = T)), 
+      formatter = fmt_number,
+      decimals = 0
+    ) %>% 
+    sub_missing(missing_text = ".") %>% 
+    tab_header(
+      title = glue::glue("SUMMARY OF OU SITE ATTRIBUTES")
+    ) %>% 
+    gt_theme_phc() %>% 
+    tab_options(row_group.font.weight = "bold",
+                row_group.font.size = px(14),
+                data_row.padding = px(1),
+                table.width = pct(75)
+                ) %>% 
+    tab_source_note(
+      source_note = gt::md(glue::glue("DATIM Data Alignment Activity Attribute Data 2023"))
+    ) %>% 
+    tab_options(
+      source_notes.font.size = px(10)) %>% 
+    gtsave(filename = glue("Images/daa_site_overall_summary.png"))
+    
+  
